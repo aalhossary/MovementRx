c542e4f00b49c14b241a19a174168061b3cd5d2d
Amr ALHOSSARY <amr_alhossary@hotmail.com>
2022-07-09 09:25:46 +0800

    Version 0.4.2

--- spmclient/ui/gui/xml/ui_about_dialogue.py
+++ spmclient/ui/gui/xml/ui_about_dialogue.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ui_about_dialogue.ui'
+# Form implementation generated from reading ui file '.\ui_about_dialogue.ui'
 #
 # Created by: PyQt5 UI code generator 5.9.2
 #
@@ -46,7 +46,7 @@ class Ui_About(object):
     def retranslateUi(self, About):
         _translate = QtCore.QCoreApplication.translate
         About.setWindowTitle(_translate("About", "Dialog"))
-        self.label.setText(_translate("About", "<html><head/><body><p align=\"center\"><span style=\" font-size:14pt; font-weight:600;\">MovementRx 0.4.1</span></p><p align=\"center\"><span style=\" font-size:10pt;\">Developped in </span><a href=\"http://rris.ntu.edu.sg/\"><span style=\" font-size:10pt; text-decoration: underline; color:#0000ff;\">Rehabilitation Research Institute of Singapore (RRIS)</span></a></p><p align=\"center\"><span style=\" font-size:10pt;\">Application credit goes to</span></p></body></html>"))
+        self.label.setText(_translate("About", "<html><head/><body><p align=\"center\"><span style=\" font-size:14pt; font-weight:600;\">MovementRx 0.4.2</span></p><p align=\"center\"><span style=\" font-size:10pt;\">Developped in </span><a href=\"http://rris.ntu.edu.sg/\"><span style=\" font-size:10pt; text-decoration: underline; color:#0000ff;\">Rehabilitation Research Institute of Singapore (RRIS)</span></a></p><p align=\"center\"><span style=\" font-size:10pt;\">Application credit goes to</span></p></body></html>"))
 
 from spmclient import resources_rc
 
